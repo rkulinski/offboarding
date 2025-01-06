c1ec315752a045b3726958da954df364dbf73c21
robert <robert.kulinski93@gmail.com>
2025-01-06 14:11:20 +0100

    chore: lint and format for api

---
+++ api/eslint.config.js
@@ -0,0 +1,15 @@
+// @ts-check
+const eslint = require("@eslint/js");
+const globals = require("globals");
+const tseslint = require("typescript-eslint");
+const pluginJs = require("@eslint/js");
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+
+module.exports = tseslint.config(
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  eslint.configs.recommended,
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+);
