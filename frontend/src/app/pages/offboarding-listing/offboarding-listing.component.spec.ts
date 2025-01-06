96cd26119881afb171728a177336d3e8a2169818
robert <robert.kulinski93@gmail.com>
2025-01-06 19:37:26 +0100

    feat: add tests

--- frontend/src/app/pages/offboarding-listing/offboarding-listing.component.spec.ts
+++ frontend/src/app/pages/offboarding-listing/offboarding-listing.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OffboardingListingComponent } from './offboarding-listing.component';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 describe('OffboardingListingComponent', () => {
   let component: OffboardingListingComponent;
@@ -9,6 +12,11 @@ describe('OffboardingListingComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [OffboardingListingComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideAnimations(),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(OffboardingListingComponent);
