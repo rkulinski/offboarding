eee938b6891e2bba9599c45de5e323d964faf007
robert <robert.kulinski93@gmail.com>
2025-01-06 18:34:56 +0100

    feat: base table

--- frontend/src/app/pages/offboarding-listing/offboarding-listing.component.ts
+++ frontend/src/app/pages/offboarding-listing/offboarding-listing.component.ts
@@ -1,9 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import {
+  MatCell,
+  MatHeaderCell,
+  MatHeaderRow,
+  MatRow,
+  MatTable,
+} from '@angular/material/table';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-offboarding-listing',
-  imports: [],
+  imports: [
+    MatFormField,
+    MatTable,
+    MatHeaderCell,
+    MatHeaderRow,
+    MatCell,
+    MatRow,
+    FormsModule,
+    MatLabel,
+  ],
   templateUrl: './offboarding-listing.component.html',
   styleUrl: './offboarding-listing.component.scss',
 })
-export class OffboardingListingComponent {}
+export class OffboardingListingComponent implements OnInit {
+  employees = [];
+  searchQuery = '';
+
+  // constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit(): void {
+    this.loadEmployees();
+  }
+
+  loadEmployees(): void {
+    // this.employeeService.getEmployees().subscribe((data) => {
+    //   this.employees = data;
+    // });
+  }
+
+  filterEmployees() {
+    return this.employees;
+  }
+}
